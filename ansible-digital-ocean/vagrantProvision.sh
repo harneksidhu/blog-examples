28f104002af2c8ab0cbedaff98dc188251313067
Harnek Sidhu <h6sidhu@gmail.com>
2016-07-23 17:38:55 -0400

    Added Vagrantfile and .gitignore

---
+++ ansible-digital-ocean/vagrantProvision.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install build-essential libssl-dev libffi-dev python-dev python-pip -y
+sudo pip install --upgrade setuptools
+sudo pip install ansible
+sudo pip install 'dopy>=0.3.5,<=0.3.5'
