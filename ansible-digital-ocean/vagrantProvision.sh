99bf36df68910ebf031e3e07e36aaaafeb9c787a
Harnek Sidhu <h6sidhu@gmail.com>
2016-07-31 12:49:50 -0400

    Fixed the vagrant provisioner and included a README

--- ansible-digital-ocean/vagrantProvision.sh
+++ ansible-digital-ocean/vagrantProvision.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install build-essential libssl-dev libffi-dev python-dev python-pip -y
-sudo pip install --upgrade setuptools
-sudo pip install ansible
-sudo pip install 'dopy>=0.3.5,<=0.3.5'
+pip install --upgrade --user setuptools
+pip install --user ansible
+pip install --user 'dopy>=0.3.5,<=0.3.5'
+echo export PATH=$PATH:/home/vagrant/.local/bin/ > ~/.profile
